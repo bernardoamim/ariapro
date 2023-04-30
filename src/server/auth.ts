8ed3e2c4c63539665ed1cab9dbd89e506b123373
bernardo.amim <bernardo.amim@mercatus.com>
2023-04-30 04:00:27 -0300

    fixing env variables

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -6,7 +6,7 @@ import {
 } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { env } from '~/env.mjs'
+// import { env } from '~/env.mjs'
 import { prisma } from '~/server/db'
 
 /**
